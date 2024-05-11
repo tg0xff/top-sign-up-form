a4d7448c450c77f9d53929f2710c93701447726e
Selene Tamez Galván <tg.sln255@gmail.com>
2024-05-10 18:09:18 -0600

    Show error message when passwords don't match

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#registration");
 const pass = form.querySelector("#password");
 const passConfirm = form.querySelector("#password_confirmation");
+const output = form.querySelector("output");
 
 form.addEventListener("change", (e) => {
   if (
@@ -10,10 +11,12 @@ form.addEventListener("change", (e) => {
     if (pass.value === passConfirm.value) {
       pass.setCustomValidity("");
       passConfirm.setCustomValidity("");
+      output.classList.remove("no-match");
     } else {
       const message = "Passwords do not match.";
       pass.setCustomValidity(message);
       passConfirm.setCustomValidity(message);
+      output.classList.add("no-match");
     }
   }
 });
